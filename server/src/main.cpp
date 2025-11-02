34fba3efe3ac7381cafd0e49daf49c0f5d249615
hxx0r <138973088+hxx0r@users.noreply.github.com>
2025-11-02 21:18:56 +0300

    Add files via upload

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,50 +1,16 @@
+// server/src/main.cpp
 #include <boost/asio.hpp>
 #include <iostream>
-#include <thread>
-#include <vector>
+#include <memory>
+#include "session.h"  // Подключаем наш новый заголовок
 
 using boost::asio::ip::tcp;
-
-class Session : public std::enable_shared_from_this<Session> {
-public:
-    Session(tcp::socket socket) : socket_(std::move(socket)) {}
-    
-    void start() {
-        do_read();
-    }
-
-private:
-    void do_read() {
-        auto self(shared_from_this());
-        socket_.async_read_some(boost::asio::buffer(data_, max_length),
-            [this, self](boost::system::error_code ec, std::size_t length) {
-                if (!ec) {
-                    std::cout << "Received: " << std::string(data_, length) << std::endl;
-                    // Просто отправляем обратно
-                    do_write(length);
-                }
-            });
-    }
-    
-    void do_write(std::size_t length) {
-        auto self(shared_from_this());
-        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-                if (!ec) {
-                    do_read();
-                }
-            });
-    }
-    
-    tcp::socket socket_;
-    enum { max_length = 1024 };
-    char data_[max_length];
-};
 
 class Server {
 public:
     Server(boost::asio::io_context& io_context, short port)
         : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+        std::cout << "Server started on port " << port << std::endl;
         do_accept();
     }
 
@@ -53,7 +19,10 @@ private:
         acceptor_.async_accept(
             [this](boost::system::error_code ec, tcp::socket socket) {
                 if (!ec) {
+                    std::cout << "New client connected" << std::endl;
                     std::make_shared<Session>(std::move(socket))->start();
+                } else {
+                    std::cout << "Accept error: " << ec.message() << std::endl;
                 }
                 do_accept();
             });
